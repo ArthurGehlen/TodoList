e0495963a3c3f13d3396378d2bc9f01053afb605
Arthur Gehlen <1arthurgehlen@gmail.com>
2025-03-19 20:13:09 -0300

    Ajuste no tamanho da fonte da task para telas menores, e troca do texto do botão "deletar" para um ícone.

--- script/script.js
+++ script/script.js
@@ -26,7 +26,7 @@ function create_task(name, is_bookmarked) {
     let delete_button = document.createElement("button");
 
     task_name.innerText = name;
-    delete_button.innerText = "Delete";
+    delete_button.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e3e3e3"><path d="M280-120q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520ZM360-280h80v-360h-80v360Zm160 0h80v-360h-80v360ZM280-720v520-520Z"/></svg>`;
 
     let bookmark_empty = `<svg xmlns="http://www.w3.org/2000/svg" height="26px" viewBox="0 0 24 24" width="26px" fill="black">
         <path d="M17 3H7c-1.1 0-2 .9-2 2v16l7-3 7 3V5c0-1.1-.9-2-2-2z" stroke="black" stroke-width="2" fill="none"/>
